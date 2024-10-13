bb276728155eaee20486f3c6fbb417a10bd995b4
Carlos Chao <chao@factory.generalsoftwareinc.com>
2024-10-12 22:26:33 -0400

    feat: update instruction section styles

--- src/sections/Landing/components/Instructions/Step.tsx
+++ src/sections/Landing/components/Instructions/Step.tsx
@@ -5,15 +5,25 @@ export type StepProps = {
   icon: ReactNode
   description: string
   image: string
+  bgColor: string
 }
 
-export default function Step({ title, icon, description, image }: StepProps) {
+export default function Step({
+  title,
+  icon,
+  description,
+  image,
+  bgColor,
+}: StepProps) {
   return (
     <div
-      className="aspect-square bg-red-400 bg-cover bg-center rounded-3xl text-center text-white flex flex-col p-4"
-      style={{ backgroundImage: `url(${image})` }}
+      className="aspect-square bg-cover bg-center rounded-xl text-center text-white flex flex-col p-4 hover:bg-right transition-all duration-600 hover:shadow-xl hover:shadow-red-200 step-container"
+      style={{
+        backgroundImage: `url(${image})`,
+        backgroundColor: bgColor,
+      }}
     >
-      <span className="text-3xl">{title}</span>
+      <span className="text-2xl font-medium">{title}</span>
       <div className="flex-grow grid place-content-center">{icon}</div>
       <span>{description}</span>
     </div>
--- src/sections/Landing/components/Instructions/index.tsx
+++ src/sections/Landing/components/Instructions/index.tsx
@@ -6,30 +6,33 @@ export default function Instructions() {
     {
       key: 1,
       title: 'Upload your Image',
-      icon: <CloudUploadIcon size={100} />,
+      icon: <CloudUploadIcon size={75} strokeWidth={1} />,
       description: 'Take a full-body photo or upload an existing one',
       image: '/images/upload-image.png',
+      bgColor: '#893780',
     },
     {
       key: 2,
       title: 'Choose your style',
-      icon: <ShirtIcon size={100} />,
+      icon: <ShirtIcon size={75} strokeWidth={1} />,
       description: 'Select an style from our list or define a new one',
       image: '/images/style-image.png',
+      bgColor: '#A63271',
     },
     {
       key: 3,
       title: 'See the magic',
-      icon: <SparklesIcon size={100} />,
+      icon: <SparklesIcon size={75} strokeWidth={1} />,
       description: 'Our AI will show you wearing the selected outfit',
       image: '/images/result-image.png',
+      bgColor: '#CD2C5F',
     },
   ]
 
   return (
-    <div className="bg-gray-200 p-2 lg:p-8 mt-24">
+    <div className="bg-gray-200 p-2 lg:py-14 mt-24">
       <div className="container mx-auto mb-8 px-2 lg:px-16">
-        <h2 className="text-5xl font-bold mb-16">How Does It Works?</h2>
+        <h2 className="text-4xl font-bold mb-16">How Does It Works?</h2>
 
         <div className="grid lg:grid-cols-3 gap-4 lg:gap-16 mt-4">
           {steps.map(({ key, ...step }) => (
@@ -38,8 +41,8 @@ export default function Instructions() {
         </div>
 
         <div className="flex justify-center mt-16">
-          <button className="bg-red-400 text-white py-4 px-8 rounded-full hover:bg-red-300 transition duration-300 text-xl uppercase">
-            Try it now!
+          <button className="relative  bg-foreground  text-white py-4 px-8 rounded-full hover:bg-accent-foreground transition duration-300 text-xl">
+            Style Your Photos!
           </button>
         </div>
       </div>
