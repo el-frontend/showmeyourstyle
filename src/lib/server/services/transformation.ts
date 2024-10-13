e2a308839400666ad6b430fd58d433de5f910907
Carlos Chao <chao@factory.generalsoftwareinc.com>
2024-10-12 22:43:54 -0400

    feat: create transformation service with get and create

---
+++ src/lib/server/services/transformation.ts
@@ -0,0 +1,38 @@
+'use server'
+
+import { eq } from 'drizzle-orm'
+import { db } from './../db/db'
+
+import { TransformationsTable } from '../db/schemas'
+
+export const getTransformations = async (userId: string) => {
+  try {
+    return await db
+      .select()
+      .from(TransformationsTable)
+      .where(eq(TransformationsTable.userId, userId))
+  } catch (error) {
+    console.error(error)
+    return []
+  }
+}
+
+export const createTransformation = async (
+  userId: string,
+  prompt: string,
+  url: string
+) => {
+  try {
+    return await db
+      .insert(TransformationsTable)
+      .values({
+        userId,
+        prompt,
+        url,
+      })
+      .returning()
+  } catch (error) {
+    console.log(error)
+    throw new Error('Failed to create transformation')
+  }
+}
