9e6953afe8615274218a4ce2315f66c009261522
Carlos Chao <chao@factory.generalsoftwareinc.com>
2024-10-17 16:47:57 -0400

    feat: update metadata configs

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -5,6 +5,33 @@ import { Metadata } from 'next'
 export const metadata: Metadata = {
   title: 'Show me your style - Dashboard',
   description: 'Unleash your style with AI-driven fashion creations',
+  keywords: [
+    'AI fashion',
+    'style',
+    'fashion creations',
+    'personalized fashion',
+    'cloudinary',
+    'midudev',
+  ],
+  authors: [
+    { name: 'Carlos Chao Cortes', url: 'https://youtube.com/@ElFrontend' },
+    { name: 'Abel Rodriguez', url: '' },
+  ],
+  robots: 'index, follow',
+  openGraph: {
+    title: 'Show me your style',
+    description: 'Unleash your style with AI-driven fashion creations',
+    url: 'https://showmeyourstyle.vercel.app',
+    type: 'website',
+    images: [
+      {
+        url: '/images/logo.svg',
+        width: 800,
+        height: 600,
+        alt: 'Show me your style',
+      },
+    ],
+  },
 }
 
 export default function RootLayout({
--- src/app/(editor)/editor/layout.tsx
+++ src/app/(editor)/editor/layout.tsx
@@ -1,8 +1,7 @@
-import Header from '@/sections/Landing/components/Header/Header'
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: 'Show me your style',
+  title: 'Show me your style - Editor',
   description: 'Unleash your style with AI-driven fashion creations',
   keywords: [
     'AI fashion',
@@ -16,7 +15,6 @@ export const metadata: Metadata = {
     { name: 'Carlos Chao Cortes', url: 'https://youtube.com/@ElFrontend' },
     { name: 'Abel Rodriguez', url: '' },
   ],
-  robots: 'index, follow',
   openGraph: {
     title: 'Show me your style',
     description: 'Unleash your style with AI-driven fashion creations',
@@ -33,16 +31,10 @@ export const metadata: Metadata = {
   },
 }
 
-export default function LandingLayout({
+export default function EditorLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
-  return (
-    <div className="min-h-screen bg-background relative overflow-x-hidden">
-      <Header />
-      {/* <HalloweenTheme /> */}
-      {children}
-    </div>
-  )
+  return <>{children}</>
 }
